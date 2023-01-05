158c399038e8592fdb1345e6bb765bfdc40530b2
jasmine <jasminesimxy@gmail.com>
2023-01-06 00:38:59 +1100

    added back instructions for team member

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// jasmine: landing page - search button - event listener 
+
+// jasmine: hide landing page - show content page
+
+// chris: history list - retrieve data from local storage - local browser
+
+// harry: information list - fetch data from marvel api with api KeyboardEvent
+
+// chenghao: videolist - fetch data from youtube api with api key
