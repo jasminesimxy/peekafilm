c6bcc94ced93e6a8d953902da206545a11cc4f6d
jasmine <jasminesimxy@gmail.com>
2023-01-06 02:02:05 +1100

    created variables for landing and content page containers in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
+var landingContainer = document.getElementById("landing");
+var contentContainer = document.getElementById("contentpage")
+
 // jasmine: landing page - search button - event listener 
 
-// jasmine: hide landing page - show second page
+
+
+
+// jasmine: second page 
+
+function displayContent () {
+    //hide landing page 
+    landingContainer.style.display="none";
+    contentContainer.style.display="block";
+
+    
+}
 
 //chris : history list - retrieve data from local storage - local browser
 
 //harry: information list - fetch data from marvel api with api key
 
 // chenghao: video list - fetch data from youtube api with api key 
+
+
+document.getElementById("").addEventListener('click', ); // function unknown yet - 
