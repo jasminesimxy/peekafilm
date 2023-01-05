71db7ccdf0c7557499de18b09c074fadb3d7ac8c
jasmine <jasminesimxy@gmail.com>
2023-01-06 01:33:26 +1100

    added styling to landing using bulma css framework

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,5 @@
 //chris : history list - retrieve data from local storage - local browser
 
 //harry: information list - fetch data from marvel api with api key
-dfgjkhfdgdf
 
 // chenghao: video list - fetch data from youtube api with api key 
