7040b302b1d9b17d7b267907a44e1064afcb1d21
Beurre-moutarde <christopher.m.lebreton@gmail.com>
2023-01-07 02:28:50 +1100

    added the history list in HTML, CSS, JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,52 @@
 // jasmine: hide landing page - show content page
 
 // chris: history list - retrieve data from local storage - local browser
+// get the search form and the search history list
+const searchForm = document.getElementById('search-form');
+const searchHistoryList = document.getElementById('search-history');
 
+// add an event listener to the search form
+searchForm.addEventListener('submit', (event) => {
+  // prevent the form from submitting
+  event.preventDefault();
+
+  // get the input field
+  const inputField = event.target.elements['0'];
+  // get the search term
+  const searchTerm = inputField.value;
+
+  // check if the search term is not empty
+  if (searchTerm.trim() !== '') {
+    // add the search term to the search history list
+    searchHistoryList.innerHTML += `<li>${searchTerm}</li>`;
+
+    // add the search term to Local Storage
+    addToLocalStorage(searchTerm);
+  }
+});
+
+// add a function to add the search term to Local Storage
+function addToLocalStorage(searchTerm) {
+  // check if Local Storage is supported by the browser
+  if (typeof(Storage) !== 'undefined') {
+    // get the current search history from Local Storage
+    let searchHistory = localStorage.getItem('searchHistory');
+    // check if there is a search history in Local Storage
+    if (searchHistory !== null) {
+      // parse the search history from JSON to an array
+      searchHistory = JSON.parse(searchHistory);
+    } else {
+      // create an empty search history array
+      searchHistory = [];
+    }
+    // add the search term to the search history array
+    searchHistory.push(searchTerm);
+    // stringify the search history array and add it to Local Storage
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+  } else {
+    // display an error message if Local Storage is not supported
+    searchHistoryList.innerHTML = '<li>Sorry, your browser does not support Local Storage</li>';
+  }};
 // harry: information list - fetch data from marvel api with api KeyboardEvent
 
 // chenghao: videolist - fetch data from youtube api with api key
