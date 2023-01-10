1739ea132a9a4ce052fbb69954d7da94c57ab0a0
jasmine <jasminesimxy@gmail.com>
2023-01-10 19:42:37 +1100

    problem fixing now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,3 @@
-// jasmine: landing page - search button - event listener 
-var landingContainer = document.getElementById("landing");
-var contentContainer = document.getElementById("contentpage")
-
-// jasmine: landing page - search button - event listener 
-
-
-
-
-// jasmine: second page 
-
-function displayContent () {
-    //hide landing page 
-    landingContainer.style.display="none";
-    contentContainer.style.display="block";
-
-}
-
-// jasmine: hide landing page - show content page
 
 // chris: history list - retrieve data from local storage - local browser
 // get the search form and the search history list
@@ -27,6 +8,8 @@ const searchHistoryList = document.getElementById('search-history');
 searchForm.addEventListener('submit', (event) => {
   // prevent the form from submitting
   event.preventDefault();
+
+
 
   // get the input field
   const inputField = event.target.elements['0'];
@@ -80,6 +63,26 @@ function addToLocalStorage(searchTerm) {
     searchHistoryList.innerHTML = '<li>Sorry, your browser does not support Local Storage</li>';
   }};
 // harry: information list - fetch data from marvel api with api KeyboardEvent
+
+
+const MARVEL_BASE_URL = 'https://gateway.marvel.com:443/v1/public';
+const MARVEL_API_KEY = '24e377359e91ec437d1b5963bd2dc195';
+
+
+
+function searchCharacters(query) {
+  const url = `${MARVEL_BASE_URL}/characters?ts=1&nameStartsWith=${query}&apikey=${MARVEL_API_KEY}&hash=667ae841fa34b73b2b37dd1c1df1e89e`;
+  return fetch(url)
+    .then(response => response.json())
+    
+}
+
+searchCharacters('spider')
+  .then(characters => console.log(characters));
+
+const searchResults = document.getElementById("search-results")
+let result = "";
+
 
 // chenghao: videolist - fetch data from youtube api with api key
 
