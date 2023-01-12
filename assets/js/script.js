1cd65ee625e269def41b0a224c7bef0773f35709
Harry Katiforis <harry@Harrys-Air.modem>
2023-01-12 21:30:09 +1100

    combined youtube and marvel api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var btnEl = document.querySelector('#search-button');
-var searchInputEl = document.querySelector('#search-input');
-var heroInputEl = document.getElementById('hero-input');
+var searchInputEl = document.querySelector('#superheroName');
 // (tutor) james says to move this out of the function and into the global scope
 var videoListEl = document.querySelector('#video-container')
 // chris: history list - retrieve data from local storage - local browser
@@ -9,8 +8,6 @@ const searchForm = document.getElementById('search-form');
 const searchHistoryList = document.getElementById('search-history');
 
 
-const MARVEL_BASE_URL = 'https://gateway.marvel.com:443/v1/public';
-const MARVEL_API_KEY = '24e377359e91ec437d1b5963bd2dc195';
 
 
 
@@ -41,6 +38,16 @@ function addToLocalStorage(searchTerm) {
 // harry: information list - fetch data from marvel api with api KeyboardEvent
 
 
+const MARVEL_BASE_URL = 'https://gateway.marvel.com:443/v1/public';
+const MARVEL_API_KEY = '24e377359e91ec437d1b5963bd2dc195';
+const youtubeApiKey = "AIzaSyDC-TEGQQzeXYzTXJNiOI1ckI58hGEqZg4";
+const marvelEntertainmentID ="UCvC4D8onUfXzvjTOM-dBfEA";
+var searchInputEl = document.querySelector('#superheroName');
+var videoListEl = document.querySelector('#video-container');
+var maxResults = "5";
+var heroBtn = document.querySelector('#search');
+const searchResults = document.getElementById("search-results");
+let result = "";
 function searchCharacters(e,query) {
   e.preventDefault()
   document.getElementById('search-results').innerHTML = ''
@@ -48,8 +55,6 @@ function searchCharacters(e,query) {
   const url = `${MARVEL_BASE_URL}/characters?ts=1&nameStartsWith=${query}&apikey=${MARVEL_API_KEY}&hash=667ae841fa34b73b2b37dd1c1df1e89e`;
   return fetch(url)
     .then(response => response.json())
-
-
     .then(data => {
       let characters = data.data.results;
       console.log(characters)
@@ -59,130 +64,47 @@ function searchCharacters(e,query) {
           let h1 = document.createElement('h1')
       h1.textContent = characters[i].name
       document.getElementById('search-results').appendChild(h1)
-
       let h2 = document.createElement('h1')
       h2.textContent = characters[i].description
       document.getElementById('search-results').appendChild(h2)
-     
       let img = document.createElement('img')
       img.src = characters[i].thumbnail.path + '.jpg'
       document.getElementById('search-results').appendChild(img)
       }
-      
       var searchInput = query;
     console.log(searchInput);
-
-    
-    })
-     
-     
-  };
-
-var heroBtn = document.querySelector('#search')
-heroBtn.addEventListener('click', searchCharacters)
-
-// searchCharacters('spider')
-//   .then(characters => console.log(characters));
-
-
-
-
-
-const searchResults = document.getElementById("search-results")
-let result = "";
-
-
-
-// chenghao: videolist - fetch data from youtube api with api key
-
-function getYoutubeApi(){
-    videoListEl.replaceChildren();
-    
-    console.log(('inside getYoutubeApi'));
-    
-    var heroStr = heroInputEl.value;
-    console.log(heroStr);
-    // (tutor) james: is this API endpoint correctly fetching the data you want?
-    var requestUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCvC4D8onUfXzvjTOM-dBfEA&maxResults=5&order=date&q=' + heroStr + '&key=AIzaSyDC-TEGQQzeXYzTXJNiOI1ckI58hGEqZg4';
-    
-    
-    fetch(requestUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-
-        var videoID = data.items[0].id.videoId;
-        console.log(videoID) //videoId
-
-        var videoImage = data.items[0].snippet.thumbnails.default.url;
-        console.log(videoImage) //image src
-
-        var videoEl = document.createElement('a');
-        var videoImageEl = document.createElement('img');
-        videoImageEl.src = videoImage;
-
-        //videoEl.setAttribute("href","https://www.youtube.com/watch?v=" + videoID);
-        videoEl.setAttribute("href","https://www.youtube.com/embed/" + videoID);
-
-        //videoEl.setAttribute("href","https://www.googleapis.com/youtube/v3/videos?part=player&id=" + videoID + "&key=AIzaSyDC-TEGQQzeXYzTXJNiOI1ckI58hGEqZg4");
-        
-        videoEl.append(videoImageEl);
-
-        
-    
-
-        console.log(videoEl);
-        console.log(videoListEl);
-        console.log(videoImageEl);
-
-        videoListEl.appendChild(videoEl);
-
-        // now run the searchCharacter function?
-        searchCharacters(heroStr)
-        .then(function(data) {
-          console.log('Marvel data is', data);
-        });
+    getYoutubeApi();
     })
 }
-
-btnEl.addEventListener('click',getYoutubeApi);
-
-// add an event listener to the search form
-searchForm.addEventListener('submit', (event) => {
-  // prevent the form from submitting
-  event.preventDefault();
-
-
-
-  // get the input field
-  const inputField = event.target.elements['0'];
-  // get the search term
-  const searchTerm = inputField.value;
-
-
- btnEl.addEventListener('click',getApi);
-
-  // check if the search term is not empty
-  if (searchTerm.trim() !== '') {
-    // add the search term to the search history list
-    searchHistoryList.innerHTML += `<li>${searchTerm}</li>`;
-
-    // add the search term to Local Storage
-    addToLocalStorage(searchTerm);
-  }
-});
-// add an event listener to the search history list
-searchHistoryList.addEventListener('click', (event) => {
-    // get the clicked element
-    const clickedElement = event.target;
-    // check if the clicked element is a list item
-    if (clickedElement.tagName === 'LI') {
-      // get the text of the list item
-      const searchTerm = clickedElement.textContent;
-      // display the search term in an alert
-      displaySearchInfo(searchTerm);
-      // you can retrieve and display the information of the search term here
+function getYoutubeApi(){
+  //videoListEl.removeChild();
+  var searchInput = searchInputEl.value;
+  console.log(searchInput);
+  var requestUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId='+ marvelEntertainmentID +'&maxResults=' + maxResults +'&q=' + searchInput + '&key=' + youtubeApiKey;
+  fetch(requestUrl)
+  .then(function(response){
+      if(!response.ok){
+        throw response.json();
+      }
+      return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+    console.log(data.items.length)
+    var child = videoListEl.lastElementChild;
+    while (child) {
+      videoListEl.removeChild(child);
+        child = videoListEl.lastElementChild;
     }
-});
+    for (var i = 0; i < data.items.length; i++){
+      var videoID = data.items[i].id.videoId;
+      var iframeEl = document.createElement('iframe');
+      iframeEl.setAttribute("src","https://www.youtube.com/embed/" + videoID);
+      iframeEl.setAttribute("allowfullscreen","1");
+      iframeEl.setAttribute("height","300px");
+      iframeEl.setAttribute("width","600px");
+      videoListEl.appendChild(iframeEl)
+    }
+  })
+}
+heroBtn.addEventListener('click', searchCharacters);
